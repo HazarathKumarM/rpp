448ab9cb6c11974e35c4b89c81e024810c462b3a
Abishek <52214183+r-abishek@users.noreply.github.com>
2025-01-14 17:27:18 -0800

    RPP Rain augmentation - HOST and HIP implementation (#463)
    * Add Intial u8 implementation for Rain
    
    * Add I8 implementation and Changes based on the Review comments
    
    * Initial HIP implementation
    Add F32 and F16 Tensor Implementation
    
    * Add test case for Rain in HIP test suite
    code cleanup
    
    * minor code cleanup
    
    * Modified func names and removed unnecessary code
    
    * Resolve Review comments
    
    * replaced pinned memory with HIP memory for Rain Layer computation
    
    * Modified RGB Rain Mask to planar Rain Mask in HIP
    
    * Address review comments
    
    * Add Rain compute function
    
    * Add version changes and Resolve review comments
    
    * fix build warnings
    
    * Fix the outputs of f16 toggle variants
    
    * Revert Rain width changes
    
    * Fix pln3 outputs for u8 and i8 bitdepths
    
    * Resolve review comments
    
    * Modified load and store routines for planar cases
    
    * Resolve review comments
    
    * Modify docs image
    
    * Fix versioning
    
    * Updates to 1.9.10 including rain feature
    
    ---------
    
    Co-authored-by: HazarathKumarM <hazarathkumar@multicorewareinc.com>
    Co-authored-by: Kiriti Gowda <kiritigowda@gmail.com>
    Co-authored-by: Srihari-mcw <srihari@multicorewareinc.com>

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -63,7 +63,7 @@ int main(int argc, char **argv)
     bool additionalParamCase = (testCase == 8 || testCase == 21 || testCase == 23|| testCase == 24 || testCase == 28 || testCase == 40 || testCase == 41 || testCase == 49 || testCase == 54 || testCase == 79);
     bool kernelSizeCase = (testCase == 40 || testCase == 41 || testCase == 49 || testCase == 54);
     bool dualInputCase = (testCase == 2 || testCase == 30 || testCase == 33 || testCase == 61 || testCase == 63 || testCase == 65 || testCase == 68);
-    bool randomOutputCase = (testCase == 6 || testCase == 8 || testCase == 10 || testCase == 84 || testCase == 49 || testCase == 54);
+    bool randomOutputCase = (testCase == 6 || testCase == 8 || testCase == 10 || testCase == 11 || testCase == 84 || testCase == 49 || testCase == 54);
     bool nonQACase = (testCase == 24 || testCase == 28 || testCase == 54);
     bool interpolationTypeCase = (testCase == 21 || testCase == 23 || testCase == 24|| testCase == 28 || testCase == 79);
     bool reductionTypeCase = (testCase == 87 || testCase == 88 || testCase == 89 || testCase == 90 || testCase == 91);
@@ -427,7 +427,7 @@ int main(int argc, char **argv)
     void *d_interDstPtr;
     if(testCase == 5)
         CHECK_RETURN_STATUS(hipHostMalloc(&d_interDstPtr, srcDescPtr->strides.nStride * srcDescPtr->n * sizeof(Rpp32f)));
-    
+
     Rpp32f *perspectiveTensorPtr = NULL;
     if(testCase == 28)
         CHECK_RETURN_STATUS(hipHostMalloc(&perspectiveTensorPtr, batchSize * 9 * sizeof(Rpp32f)));
@@ -698,6 +698,25 @@ int main(int argc, char **argv)
                     startWallTime = omp_get_wtime();
                     if (inputBitDepth == 0 || inputBitDepth == 1 || inputBitDepth == 2 || inputBitDepth == 5)
                         rppt_fog_gpu(d_input, srcDescPtr, d_output, dstDescPtr, intensityFactor, greyFactor, roiTensorPtrSrc, roiTypeSrc, handle);
+                    else
+                        missingFuncFlag = 1;
+
+                    break;
+                }
+                case 11:
+                {
+                    testCaseName = "rain";
+
+                    Rpp32f rainPercentage = 7;
+                    Rpp32u rainHeight = 6;
+                    Rpp32u rainWidth = 1;
+                    Rpp32f slantAngle = 0;
+                    for (int i = 0; i < batchSize; i++)
+                        alpha[i] = 0.4;
+
+                    startWallTime = omp_get_wtime();
+                    if (inputBitDepth == 0 || inputBitDepth == 1 || inputBitDepth == 2 || inputBitDepth == 5)
+                        rppt_rain_gpu(d_input, srcDescPtr, d_output, dstDescPtr, rainPercentage, rainWidth, rainHeight, slantAngle, alpha, roiTensorPtrSrc, roiTypeSrc, handle);
                     else
                         missingFuncFlag = 1;
 
@@ -1656,7 +1675,7 @@ int main(int argc, char **argv)
                 // Calculate exact dstROI in XYWH format for OpenCV dump
                 if (roiTypeSrc == RpptRoiType::LTRB)
                     convert_roi(roiTensorPtrDst, RpptRoiType::XYWH, dstDescPtr->n);
-                    
+
                 // Check if the ROI values for each input is within the bounds of the max buffer allocated
                 RpptROI roiDefault;
                 RpptROIPtr roiPtrDefault = &roiDefault;
