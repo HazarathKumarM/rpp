ce796471879e9df027ca003d403f6fe04e7df943
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2025-03-18 03:53:16 +0000

    update the jpeg doxygen comments

--- include/rppt_tensor_geometric_augmentations.h
+++ include/rppt_tensor_geometric_augmentations.h
@@ -769,6 +769,28 @@ RppStatus rppt_warp_perspective_host(RppPtr_t srcPtr, RpptDescPtr srcDescPtr, Rp
 RppStatus rppt_warp_perspective_gpu(RppPtr_t srcPtr, RpptDescPtr srcDescPtr, RppPtr_t dstPtr, RpptDescPtr dstDescPtr, Rpp32f *perspectiveTensor, RpptInterpolationType interpolationType, RpptROIPtr roiTensorPtrSrc, RpptRoiType roiType, rppHandle_t rppHandle);
 #endif // GPU_SUPPORT
 
+/*!
+ * \brief JPEG Compression Distortion on the HIP backend
+ * \details This function simulates JPEG compression distortion on an image tensor in hip memory.
+ *          It introduces artifacts seen in lossy JPEG compression by converting the image to the frequency domain using the Discrete Cosine Transform (DCT),
+ *          applying quantization, and then reconstructing the image using the inverse DCT (IDCT).
+ *          This process introduces compression-related distortions similar to those in JPEG images.
+ * \image html img150x150.png Sample Input
+ * \image html geometric_augmentations_jpeg_compression_distortion_img150x150.png Sample Output
+ * \param [in] srcPtr source tensor in HIP memory
+ * \param [in] srcGenericDescPtr source tensor descriptor
+ * \param [out] dstPtr source tensor in HIP memory
+ * \param [in] dstGenericDescPtr destination tensor descriptor
+ * \param [in] roiTensorPtrSrc ROI data in HIP memory, for each image in source tensor (2D tensor of size batchSize * 4, in either format - XYWH(xy.x, xy.y, roiWidth, roiHeight) or LTRB(lt.x, lt.y, rb.x, rb.y))
+ * \param [in] roiType ROI type used (RpptRoiType::XYWH or RpptRoiType::LTRB)
+ * \param [in] rppHandle RPP HIP handle created with <tt>\ref rppCreate()</tt>
+ * \return A <tt> \ref RppStatus</tt> enumeration.
+ * \retval RPP_SUCCESS Successful completion.
+ * \retval RPP_ERROR* Unsuccessful completion.
+ * \ingroup group_tensor_geometric
+ */
+RppStatus rppt_jpeg_compression_distortion_gpu(RppPtr_t srcPtr, RpptDescPtr srcDescPtr, RppPtr_t dstPtr, RpptDescPtr dstDescPtr, RpptROIPtr roiTensorPtrSrc, RpptRoiType roiTtype, rppHandle_t rppHandle);
+
 #ifdef __cplusplus
 }
 #endif
